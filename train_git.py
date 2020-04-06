e227813d8057760e0ae4587f41496ae77cbe05f0
Milena Kowalska <milenakowalska126@gmail.com>
2020-04-06 22:13:58 +0200

    One more commit

--- train_git.py
+++ train_git.py
@@ -1,2 +1,3 @@
-print('I learn git. This is my first version')
+print('I learn git. This is my first version. And here I make a change')
 print('Ich lerne git. Das ist meine zweite gute Version.')
+print("C'est le troisieme changement. J'apprends git")
