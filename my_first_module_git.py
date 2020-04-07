f6910c98953d12ca56cd878f50f5b36bf0e1d2a2
Milena Kowalska <milenakowalska126@gmail.com>
2020-04-07 17:18:11 +0200

    Adding some practical info

--- my_first_module_git.py
+++ my_first_module_git.py
@@ -1,2 +1,14 @@
 def addition(x,y):
     return (x,y)
+
+
+
+for x in range(100):
+    if x % 5 == 0 and x % 3 == 0:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print (x)
