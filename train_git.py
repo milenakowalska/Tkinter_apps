c5a768577dca6eeb38fd1744974a91e4432cbf71
Milena Kowalska <63208411+milenakowalska@users.noreply.github.com>
2020-04-10 07:03:52 +0200

    Delete train_git.py

--- train_git.py
+++
@@ -1,6 +0,0 @@
-print('I learn git. This is my first version. And here I make a change')
-print('Ich lerne git. Das ist meine zweite gute Version.')
-print("C'est le troisieme changement. J'apprends git")
-
-list = [x**2 for x in range(4,100) if x % 2 == 0]
-print(list)
