74391318f506711c6d5384c7be6f38de84cf897d
Milena Kowalska <milenakowalska126@gmail.com>
2020-06-17 11:19:12 +0200

    Pictures changed, solar system removed

--- Tkinter_apps.py
+++ Tkinter_apps.py
@@ -1,6 +1,5 @@
 import Math_app.math_app
 import Learn_vocabulary.vocabulary
-import Solar_system.solar
 import tkinter as tk
 import os
 
@@ -9,7 +8,7 @@ class Myroot(tk.Tk):
         super().__init__()
         self.title('Tkinter Apps!')
         self.config(background = '#ffdac3')
-        self.geometry('300x500')
+        self.geometry('300x450')
 
         self.frame= tk.Frame(self, bg = '#ffdac3', bd = 0, highlightthickness = 0)
         self.frame.grid(row = 1, pady = (40,100))
@@ -17,7 +16,6 @@ class Myroot(tk.Tk):
 
         tk.Button (self.frame, text = 'Math app', highlightbackground = '#ffa46d', highlightthickness=6,command = Math_app.math_app.main_import, pady = 20, padx = 30).pack(pady = 10, padx = 30, ipadx = 50)
         tk.Button (self.frame, text = 'Learn vocabulary', highlightbackground = '#ffa46d', highlightthickness=6,command = Learn_vocabulary.vocabulary.main_import, pady = 20, padx = 30).pack(pady = 10, padx = 30, ipadx = 27)
-        tk.Button (self.frame, text = 'Solar system', highlightbackground = '#ffa46d', highlightthickness=6 , command = Solar_system.solar.main_import, pady = 20, padx = 30).pack(pady = 10, padx = 30, ipadx = 39)
        
         self.closeb = tk.Button (self.frame, text = 'CLOSE PROGRAMM', highlightbackground = 'grey', highlightthickness=6,command = self.destroy,pady = 20, padx = 30)
         self.closeb.pack(pady = 10, ipadx = 20)
